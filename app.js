2d5d49684ae4a7c87e670c1b69d909447ab2cde6
Jon Gill <jonpgill1@gmail.com>
2022-03-04 12:07:59 +0000

    Amend deadline text

--- app.js
+++ app.js
@@ -79,7 +79,7 @@ function getRemaindingTime() {
 
   if (t < 0) {
     clearInterval(countdown);
-    deadline.innerHTML = `<h4 class="expired">sorry, this giveaway has expired!</h4>`;
+    deadline.innerHTML = `<h1 class="expired">Happy Birthday</h1>`;
   }
 }
 // countdown;
