a33bf336fb9d67560369d828af2564602d1b5c35
Jon Gill <jonpgill1@gmail.com>
2022-03-06 09:16:43 +0000

    adjust futureDate params

--- app.js
+++ app.js
@@ -30,9 +30,9 @@ let tempYear = tempDate.getFullYear();
 let tempMonth = tempDate.getMonth();
 let tempDay = tempDate.getDate();
 // months are ZERO index based;
-const futureDate = new Date(tempYear, tempMonth, tempDay + 22, 0, 0, 0);
+const futureDate = new Date(tempYear, tempMonth, tempDay + 20, 0, 0, 0);
 
-// let futureDate = new Date(2020, 3, 24, 11, 30, 0);
+// let futureDate = new Date(2022, 3, 26, 7, 00, 0);
 
 const year = futureDate.getFullYear();
 const hours = futureDate.getHours();
